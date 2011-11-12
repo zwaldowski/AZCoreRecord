1567b66e0506e317e3ba2221a194ac274f4ec73a
Zachary Waldowski <zwaldowski@gmail.com>
2011-11-12 16:15:25 -0500

    I capitulate. @pandamonia is right. :-P

--- Source/CoreData+MagicalRecord.h
+++ Source/CoreData+MagicalRecord.h
@@ -13,18 +13,6 @@
 #define kCreateNewCoordinatorOnBackgroundOperations     0
 
 #ifdef MR_LOGGING
-#define ENABLE_ACTIVE_RECORD_LOGGING
-#endif
-
-#ifdef DEBUG
-#define ENABLE_ACTIVE_RECORD_LOGGING
-#endif
-
-#ifdef MR_NO_LOGGING
-#undef ENABLE_ACTIVE_RECORD_LOGGING
-#endif
-
-#ifdef ENABLE_ACTIVE_RECORD_LOGGING
     #define ARLog(...) NSLog(@"%s(%p) %@", __PRETTY_FUNCTION__, self, [NSString stringWithFormat:__VA_ARGS__])
 #else
     #define ARLog(...)
