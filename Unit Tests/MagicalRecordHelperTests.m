5d87a5e2a15b6fc2c57566c5784ded19b40c9efa
Zachary Waldowski <zwaldowski@gmail.com>
2011-11-14 11:55:32 -0500

    Make sure the other error handler is clear to fix crashes.

--- Unit Tests/MagicalRecordHelperTests.m
+++ Unit Tests/MagicalRecordHelperTests.m
@@ -97,6 +97,7 @@
 {
 	errorHandlerWasCalled_ = NO;
 	[MagicalRecordHelpers setErrorHandlerTarget:self];
+    [MagicalRecordHelpers setErrorHandler:NULL];
 	
 	NSError *testError = [NSError errorWithDomain:@"MRTests" code:1000 userInfo:nil];
 	[MagicalRecordHelpers handleErrors:testError];
@@ -110,6 +111,7 @@
 	[MagicalRecordHelpers setErrorHandler: ^(NSError *error) {
 		[self handleErrors: error];
 	}];
+    [MagicalRecordHelpers setErrorHandlerTarget:nil];
 	
 	NSError *testError = [NSError errorWithDomain:@"MRTests" code:1000 userInfo:nil];
 	[MagicalRecordHelpers handleErrors: testError];
@@ -124,8 +126,9 @@
 	[[mockErrorHandler expect] handleErrors:testError];
 	
 	[MagicalRecordHelpers setErrorHandlerTarget:mockErrorHandler];
+	[MagicalRecordHelpers setErrorHandler:NULL];
 	[MagicalRecordHelpers handleErrors:testError];
-
+    
 	[mockErrorHandler verify];
 }
 
